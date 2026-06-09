ec26cb102e4d8e8cbd7698d0721024003d218210
neerajahirwar225 <neerajahirwar225@gmail.com>
2026-06-09 20:36:34 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+      <div id="child1"><h1>neeraj</h1></div>
+      <div id="child2"><h2>ahirwar</h2></div>
+      </div>
+      <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
